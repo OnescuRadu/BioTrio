f85255c47f67fdb9449ad23a4918670a3e7745c9
Paul Chelaru <chelarupaul1@gmail.com>
2019-05-08 16:39:36 +0200

    Save movie not-working*

--- src/main/java/dk/kea/dat18i/teamsix/biotrio/controllers/MovieController.java
+++ src/main/java/dk/kea/dat18i/teamsix/biotrio/controllers/MovieController.java
@@ -1,12 +1,15 @@
 package dk.kea.dat18i.teamsix.biotrio.controllers;
 
 import dk.kea.dat18i.teamsix.biotrio.models.Movie;
+import dk.kea.dat18i.teamsix.biotrio.models.MovieDetails;
 import dk.kea.dat18i.teamsix.biotrio.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -43,10 +46,15 @@ public class MovieController {
     }
 
     @GetMapping("/add-movie-page")
-    public String addMovie()
-    {
+    public String addMovie(Model m) {
+        m.addAttribute("movieForm", new Movie());
         return "/add-movie-page";
     }
 
+    @PostMapping("/save-movie")
+    public String saveMovie(@ModelAttribute Movie movie){
+        Movie movieInserted = movieRepo.saveMovie(movie);
+        return "redirect:/add-movie-page";
+    }
 
 }
