7630dab69eb07eb2f99f511f48aedc2424b4f9f6
radu-dev <onescuradu@yahoo.com>
2019-05-08 12:04:10 +0200

    Changed Colors Permit All Security Add Control Panel Page

--- src/main/java/dk/kea/dat18i/teamsix/biotrio/configurations/SecurityConfiguration.java
+++ src/main/java/dk/kea/dat18i/teamsix/biotrio/configurations/SecurityConfiguration.java
@@ -30,8 +30,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/movies/**", "/contact", "/about-us", "/faq", "/movie/**", "/css/**", "/images/**").permitAll()
-                .antMatchers("/control-panel/**").hasAnyRole("ADMIN", "USER")
+                /*.antMatchers("/", "/index", "/movies/**", "/contact", "/about-us", "/faq", "/movie/**", "/css/**", "/images/**").permitAll()
+                .antMatchers("/control-panel/**").hasAnyRole("ADMIN", "USER")*/
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
                 .permitAll();
         http.exceptionHandling().accessDeniedPage("/403");
